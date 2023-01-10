40b6d691571690c7e4f267a2592ff3290a1c9f6b
HunSeongPark <phunseong@gmail.com>
2023-01-10 16:45:27 +0900

    feat: IndexController

---
+++ src/main/java/com/example/action/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.action;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Hunseong-Park
+ * @date : 2023-01-10
+ */
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "ver 1";
+    }
+}
